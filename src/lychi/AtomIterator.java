6625502135b9e0487c5ec58f59df10744d0f5ff4
trung <caodac@gmail.com>
2019-05-01 16:10:46 -0400

    update graph invariant traversal

--- src/lychi/AtomIterator.java
+++ src/lychi/AtomIterator.java
@@ -16,21 +16,21 @@ import chemaxon.formats.*;
  */
 public class AtomIterator implements ListIterator<MolAtom> {
     private static final Logger logger = 
-	Logger.getLogger(AtomIterator.class.getName());
+    Logger.getLogger(AtomIterator.class.getName());
 
     static class IntPair implements Comparable<IntPair> {
-	int index, value;
-	IntPair (int index, int value) {
-	    this.index = index;
-	    this.value = value;
-	}
-	public int compareTo (IntPair ip) {
-	    int d = value - ip.value;
-	    if (d == 0) {
-		d = index - ip.index;
-	    }
-	    return d;
-	}
+    int index, value;
+        IntPair (int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+        public int compareTo (IntPair ip) {
+            int d = value - ip.value;
+            if (d == 0) {
+                d = index - ip.index;
+            }
+            return d;
+        }
         public String toString () {
             return "["+index+","+value+"]";
         }
@@ -41,71 +41,71 @@ public class AtomIterator implements ListIterator<MolAtom> {
     protected List<Integer> index;
 
     public AtomIterator (MoleculeGraph mol) {
-	setMolecule (mol);
+        setMolecule (mol);
     }
 
     public AtomIterator (MoleculeGraph mol, int[] rank) {
-	setMolecule (mol, rank);
+        setMolecule (mol, rank);
     }
-
+    
     public void setMolecule (MoleculeGraph mol) {
-	int[] rank = new int[mol.getAtomCount()];
-	mol.getGrinv(rank, MoleculeGraph.GRINV_NOHYDROGEN);
+        int[] rank = new int[mol.getAtomCount()];
+        mol.getGrinv(rank, MoleculeGraph.GRINV_NOHYDROGEN);
         //mol.calcDehydrogenizedGrinv(rank);
-	setMolecule (mol, rank);
+        setMolecule (mol, rank);
     }
-
+    
     public void setMolecule (MoleculeGraph mol, int[] r) {
-	atoms = mol.getAtomArray();
-
-	IntPair[] ip = new IntPair[atoms.length];
+        atoms = mol.getAtomArray();
+        
+        IntPair[] ip = new IntPair[atoms.length];
         int[] rank = new int[atoms.length]; // make a copy
-	for (int i = 0; i < ip.length; ++i) {
+        for (int i = 0; i < ip.length; ++i) {
             rank[i] = r[i];
-	    ip[i] = new IntPair (i, rank[i]);
-	}
-	Arrays.sort(ip);
-
-        /*
-        System.out.println(((Molecule)mol).toFormat("smiles:q"));
-        for (IntPair i : ip) {
-            System.out.println(i+": "+atoms[i.index].getSymbol());
+            ip[i] = new IntPair (i, rank[i]);
         }
+        Arrays.sort(ip);
+        
+        /*
+          System.out.println(((Molecule)mol).toFormat("smiles:q"));
+          for (IntPair i : ip) {
+          System.out.println(i+": "+atoms[i.index].getSymbol());
+          }
         */
-
-	index = new ArrayList<Integer>();
-	for (int i = 0; i < atoms.length; ++i) {
-	    dfs (mol, ip[i].index, index, rank);
-	}
-	ip = null;
-	iter = index.listIterator();
+        
+        index = new ArrayList<Integer>();
+        for (int i = 0; i < atoms.length; ++i) {
+            dfs (mol, ip[i].index, index, rank);
+        }
+        ip = null;
+        iter = index.listIterator();
     }
-
+    
     /*
      * Be careful changing this function!!!
      */
     void dfs (MoleculeGraph mol, int pos, List<Integer> index, int[] rank) {
-	if (rank[pos] < Integer.MAX_VALUE) {
-	    index.add(pos);
-	    rank[pos] = Integer.MAX_VALUE;
-	    
-	    // find the lowest non-visited atom
-	    int k = mol.getNeighborCount(pos);
-	    if (k > 0) {
-		int j = mol.getNeighbor(pos, 0);
-		for (int i = 1; i < k; ++i) {
-		    int nb = mol.getNeighbor(pos, i);
+        if (rank[pos] < Integer.MAX_VALUE) {
+            index.add(pos);
+            rank[pos] = Integer.MAX_VALUE;
+            
+            // find the lowest non-visited atom
+            int k = mol.getNeighborCount(pos);
+            if (k > 0) {
+                int j = mol.getNeighbor(pos, 0);
+                for (int i = 1; i < k; ++i) {
+                    int nb = mol.getNeighbor(pos, i);
                     if (rank[nb] == rank[j]) {
                         if (nb < j)
                             j = nb;
                     }
-		    else if (rank[nb] < rank[j]) {
-			j = nb;
-		    }
-		}
-		dfs (mol, j, index, rank);
-	    }
-	}
+                    else if (rank[nb] < rank[j]) {
+                        j = nb;
+                    }
+                }
+                dfs (mol, j, index, rank);
+            }
+        }
     }
 
     /*
@@ -122,28 +122,28 @@ public class AtomIterator implements ListIterator<MolAtom> {
      * Unsupported operations
      */
     public void add (MolAtom atom) {
-	throw new UnsupportedOperationException ("add is not supported");
+        throw new UnsupportedOperationException ("add is not supported");
     }
     public void remove () {
-	throw new UnsupportedOperationException ("remove is not supported");
+        throw new UnsupportedOperationException ("remove is not supported");
     }
     public void set (MolAtom atom) {
-	throw new UnsupportedOperationException ("set is not supported");
+        throw new UnsupportedOperationException ("set is not supported");
     }
-
+    
     public static void main (String[] argv) throws Exception {
-	for (String s : argv) {
-	    MolImporter mi = new MolImporter (s);
-	    for (Molecule m = new Molecule (); mi.read(m); ) {
-		System.out.print(m.getName()+":");
-		AtomIterator ai = new AtomIterator (m);
-		while (ai.hasNext()) {
-		    MolAtom a = ai.next();
-		    System.out.print(" "+a.getAtomMap());
-		}
-		System.out.println();
-	    }
-	}
+        for (String s : argv) {
+            MolImporter mi = new MolImporter (s);
+            for (Molecule m = new Molecule (); mi.read(m); ) {
+                System.out.print(m.getName()+":");
+                AtomIterator ai = new AtomIterator (m);
+                while (ai.hasNext()) {
+                    MolAtom a = ai.next();
+                    System.out.print(" "+a.getAtomMap());
+                }
+                System.out.println();
+            }
+        }
     }
 }
 
